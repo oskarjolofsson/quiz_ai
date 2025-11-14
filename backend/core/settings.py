88dab2bdcfed47fc584e37a4362a8609d8dcdfb3
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-14 23:05:20 +0100

    non working sql configuration backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'quizai'),
+        'USER': os.getenv('DB_USER', 'quizaiuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
