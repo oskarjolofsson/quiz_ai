941bf32109b8d22a02df3fa8059e648ba7779f7e
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-15 09:46:34 +0100

    new table

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
