b1b3ea3513a5293db5807e9f9fda119a618bd38e
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-15 10:36:35 +0100

    user model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
-from api.views import hello
+from api.views import UserListView, hello
 from django.urls import path
 
 urlpatterns = [
     path("hello/", hello, name="hello"),
+    path("users/", UserListView.as_view(), name="user-list"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,37 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rest_framework.views import APIView
+from api.services.auth.users import UserService
+
 # Create your views here.
 
 @api_view(['GET'])
 def hello(request):
     return Response({"message": "Hello, world!"})
+
+# Users
+class UserListView(APIView):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.user_service = UserService()
+    
+    def get(self, request):
+        
+        # Execute service
+        users = self.user_service.list_all_users()
+        data = self.user_service.serialize_users(users)
+        return Response(data)
+
+    def post(self, request):
+        # Get data from request
+        email = request.data["email"]
+        name = request.data["name"]
+        username = request.data["username"]
+        
+        # Execute service
+        user = self.user_service.create_user(username=username, name=name, email=email)
+        return Response({"id": user.id})
+    
+# Cards
